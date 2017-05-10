a2670298a1cd8447677f8665ba66516c7cbe7c48
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-05-10 16:21:11 -0600

    close issue #2 finish writing tests using stubs for starred follower and following pages

--- spec/features/user_can_view_list_of_starred_repos_spec.rb
+++ spec/features/user_can_view_list_of_starred_repos_spec.rb
@@ -1,16 +1,34 @@
-# require 'rails_helper'
-#
-# feature "user's starred repositories" do
-# #   it "returns account info like username and email" do
-# #
-# #   end
-# #   it "returns profile image" do
-# #
-# #   end
-# #   it "returns number of starred repos, followers, following" do
-# #
-# #   end
-# #   it "returns links to collections of starred repos, followers, following" do
-# #
-# #   end
-# end
+require 'rails_helper'
+
+feature "user's starred repositories" do
+  before :each do
+    stub_user_profile
+
+    visit root_path
+    click_link "Sign in with Github"
+  end
+  context "starting on basic profile" do
+    scenario "user can navigate to starred repo list" do
+      click_link "Starred Repos: 2"
+
+      expect(current_path).to eq(starred_repos_path)
+    end
+  end
+  context "starting on starred repo path" do
+    scenario "user can view all their starred repos" do
+      visit starred_repos_path
+
+      within(".repo_0") do
+        expect(page).to have_content("lao9/black-thursday-project")
+        expect(page).to have_content("Ruby")
+      end
+
+      within(".repo_1") do
+        expect(page).to have_content("lao9/black-thursday-project")
+        expect(page).to have_content("Ruby")
+      end
+
+      expect(page).to_not have_css(".repo_2")
+    end
+  end
+end
